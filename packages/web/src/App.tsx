55181aff7677f6fe31b098df88a562d5979eacff
LiangNiang <liangniangbaby@gmail.com>
2022-11-22 17:57:51 +0800

    🎉 (web): init web proj

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,21 @@
+import { Button, Space, Tag } from 'antd'
+
+function App() {
+  return (
+    <>
+      <Space>
+        <Tag color='success'>success</Tag>
+        <Tag color='processing'>processing</Tag>
+        <Tag color="error">error</Tag>
+        <Tag color="warning">warning</Tag>
+        <Tag color="default">default</Tag>
+      </Space>
+      <Space>
+        <Button danger>Danger</Button>
+        <Button type='primary'>Primary</Button>
+      </Space>
+    </>
+  )
+}
+
+export default App
